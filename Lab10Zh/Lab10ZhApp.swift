7019628c7d7de7a32f192f373c7e774a4a94a7c7
25267 <zhanibekamangeldiev@gmail.com>
2021-11-18 20:09:25 +0500

    Lab-10

---
+++ Lab10Zh/Lab10ZhApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab10ZhApp.swift
+//  Lab10Zh
+//
+//  Created by Zhanibek on 18.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Lab10ZhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
